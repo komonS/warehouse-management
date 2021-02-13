516122f2a88d5cd3e92376b8af9cefa2d1ee426f
komon <silwerbas@gmail.com>
2021-02-13 20:03:59 +0700

    add product function

---
+++ application/models/ProductModel.php
@@ -0,0 +1,44 @@
+<?php 
+
+/**
+ * 
+ */
+class ProductModel extends CI_model
+{
+    public function getProductShop($shopID)
+    {
+        $where = "branch.shopID = '".$shopID."'";
+		$query = $this->db->select("*")
+                     ->from("product")
+                     ->join('branch','branch.branchID = product.branchID')
+                     ->where($where)
+	                 ->get();
+	    $result = $query->result();
+	    return $result;
+    }
+
+    public function getProductBranch($branchID)
+    {
+        $where = "product.branchID = '".$branchID."'";
+		$query = $this->db->select("*")
+                     ->from("product")
+                     ->join('branch','branch.branchID = product.branchID')
+                     ->where($where)
+	                 ->get();
+	    $result = $query->result();
+	    return $result;
+    }
+
+    public function getProduct($productID)
+    {
+        $where = "product.productID = '".$productID."'";
+		$query = $this->db->select("*")
+                     ->from("product")
+                     ->join('branch','branch.branchID = product.branchID')
+                     ->where($where)
+	                 ->get();
+	    $result = $query->result();
+	    return $result;
+    }
+
+}
